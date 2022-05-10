a52ab46c2044955d7ade21eadfb08ed2a38fb75c
Maggie Yang <mf7811@gmail.com>
2022-05-10 13:16:54 +0800

    init commit

---
+++ UI.py
@@ -0,0 +1,95 @@
+# Lastest modify: 2022/05/10
+# Version: 1.0
+# Created by YiFang
+
+import calculator
+from tkinter import *
+from functools import partial
+
+funcStr = ""
+buttonWidth = 10
+buttonHeight = 5
+labelWidth = 45
+labelHeight = 6
+
+def add_str(tmpStr):
+	global funcStr
+	funcStr = funcStr + tmpStr
+	print(funcStr)
+	label['text'] = funcStr
+
+def button_equal_event():
+	global funcStr
+	inputStr = funcStr
+	inputRegex = re.compile(r'[^\d\+\-\*\/\(\).]')
+	if inputRegex.search(inputStr) or (not inputStr[-1].isdigit() and inputStr[-1] != ")"):
+		errorMSG = "Input string is abnormal, please check."
+		print(errorMSG)
+		label['text'] = errorMSG
+		funcStr = ""
+	else:
+		if "(" in inputStr:
+			strAfterHandle = calculator.handlePerentheses(inputStr)
+			outStr = calculator.calculate(strAfterHandle)
+		else:
+			outStr = calculator.calculate(inputStr)
+		print("Final Ouput: ",outStr)
+		label['text'] = "=  "+ outStr
+		funcStr = outStr
+
+def clean_event():
+	global funcStr
+	funcStr = ""
+	label['text'] = ""
+
+
+window = Tk()
+window.title("Calculator")
+window.geometry("502x600+250+200")
+
+label = Label(window, text = "", width = labelWidth, height= labelHeight, bg='#bdbdbd',font = ('Arial', 18))
+button_1 = Button(window, text = "1", command = partial(add_str,"1"), width = buttonWidth, height= buttonHeight, font = ('Arial', 15))
+button_2 = Button(window, text = "2", command = partial(add_str,"2"), width = buttonWidth, height= buttonHeight, font = ('Arial', 15))
+button_3 = Button(window, text = "3", command = partial(add_str,"3"), width = buttonWidth, height= buttonHeight, font = ('Arial', 15))
+button_4 = Button(window, text = "4", command = partial(add_str,"4"), width = buttonWidth, height= buttonHeight, font = ('Arial', 15))
+button_5 = Button(window, text = "5", command = partial(add_str,"5"), width = buttonWidth, height= buttonHeight, font = ('Arial', 15))
+button_6 = Button(window, text = "6", command = partial(add_str,"6"), width = buttonWidth, height= buttonHeight, font = ('Arial', 15))
+button_7 = Button(window, text = "7", command = partial(add_str,"7"), width = buttonWidth, height= buttonHeight, font = ('Arial', 15))
+button_8 = Button(window, text = "8", command = partial(add_str,"8"), width = buttonWidth, height= buttonHeight, font = ('Arial', 15))
+button_9 = Button(window, text = "9", command = partial(add_str,"9"), width = buttonWidth, height= buttonHeight, font = ('Arial', 15))
+button_0 = Button(window, text = "0", command = partial(add_str,"0"), width = 24, height= buttonHeight, font = ('Arial', 15))
+button_dot = Button(window, text = ".", command = partial(add_str,"."), width = buttonWidth, height= buttonHeight, font = ('Arial', 15))
+button_plus = Button(window, text = "+", command = partial(add_str,"+"), width = buttonWidth, height= buttonHeight, font = ('Arial', 15))
+button_minus = Button(window, text = "-", command = partial(add_str,"-"), width = buttonWidth, height= buttonHeight, font = ('Arial', 15))
+button_multiply = Button(window, text = "x", command = partial(add_str,"*"), width = buttonWidth, height= buttonHeight, font = ('Arial', 15))
+button_divide = Button(window, text = "/", command = partial(add_str,"/"), width = buttonWidth, height= buttonHeight, font = ('Arial', 15))
+button_leftperen = Button(window, text = "(", command = partial(add_str,"("), width = buttonWidth, height= buttonHeight, font = ('Arial', 15))
+button_rightperen = Button(window, text = ")", command = partial(add_str,")"), width = buttonWidth, height= buttonHeight, font = ('Arial', 15))
+button_equal = Button(window, text = "=", command = button_equal_event, width = buttonWidth, height= buttonHeight, font = ('Arial', 15))
+button_clean = Button(window, text = "C", command = clean_event, width = buttonWidth, height= buttonHeight, font = ('Arial', 15))
+
+label.place(x=0,y=0)
+button_1.place(x=0,y=210)
+button_2.place(x=125,y=210)
+button_3.place(x=250,y=210)
+button_4.place(x=0,y=300)
+button_5.place(x=125,y=300)
+button_6.place(x=250,y=300)
+button_7.place(x=0,y=390)
+button_8.place(x=125,y=390)
+button_9.place(x=250,y=390)
+button_0.place(x=0,y=480)
+button_dot.place(x=250,y=480)
+button_divide.place(x=375,y=120)
+button_multiply.place(x=375,y=210)
+button_minus.place(x=375,y=300)
+button_plus.place(x=375,y=390)
+button_equal.place(x=375,y=480)
+button_leftperen.place(x=125,y=120)
+button_rightperen.place(x=250,y=120)
+button_clean.place(x=0,y=120)
+
+
+
+window.mainloop()
+
---
+++ calculator.py
@@ -0,0 +1,110 @@
+# Lastest modify: 2022/05/07
+# Version: 1.0
+# Created by YiFang
+
+import re
+
+def multiplyDivide(opArr,numArr):
+	# print(opArr)
+	# print(numArr)
+	for i in range(0,len(numArr)):
+		if opArr[i] == "*-":
+			opArr[i] = "*"
+			numArr[i] = "-" + numArr[i]
+		if opArr[i] == "/-":
+			opArr[i] = "/"
+			numArr[i] = "-" + numArr[i]
+
+		if opArr[i] == "*":
+			tempValue = float(numArr[i-1]) * float(numArr[i])
+			numArr[i-1] = str(tempValue)
+		elif opArr[i] == "/":
+			tempValue = float(numArr[i-1]) / float(numArr[i])
+			numArr[i-1] = str(tempValue)
+		else:
+			continue
+		numArr[i] = ""
+		opArr[i] = ""
+		outStr = ""
+		for j in range(0,len(numArr)):
+			outStr = outStr + opArr[j] + numArr[j]
+		return outStr
+
+	print("The formula contains unsupported operator.\n")
+
+def plusMinus(opArr,numArr):
+	tempValue = float(numArr[0])
+	for i in range(1,len(numArr)):
+		if opArr[i] == "--":
+			opArr[i] = "+"
+		elif opArr[i] == "+-":
+			opArr[i] = "-"
+
+		if opArr[i] == "+":
+			tempValue = tempValue + float(numArr[i])
+		elif opArr[i] == "-":
+			tempValue = tempValue - float(numArr[i])
+	return str(tempValue)
+
+
+def calculate(inputStr):
+	if inputStr[0] == "-":
+		inputStr = "0" + inputStr
+	opArr = re.split("[\d.]+",inputStr)
+	numArr = re.split("[\+\-\*\/]+",inputStr)
+	if "*" in inputStr or "/" in inputStr:
+		outStr = multiplyDivide(opArr,numArr)
+		if outStr[0] == "0" and outStr[1] != ".":
+			outStr = outStr[1:]
+		try:
+			if outStr[-2:] == ".0":
+				outStr = outStr[:-2]
+			outValue = float(outStr)
+			# print("Output: ", outStr)
+			return outStr
+		except: #not number, need calculate
+			return calculate(outStr)
+	elif "+" in inputStr or "-" in inputStr:
+		outStr = plusMinus(opArr,numArr)
+		if outStr[-2:] == ".0":
+			outStr = outStr[:-2]
+		# print("Output: ", outStr)
+		return outStr
+
+def handlePerentheses(inputStr):
+	print("inputStr",inputStr)
+	subStrPattern = re.compile(r'\([\d\+\-\*\/.]*\)')
+	subStrMatch = subStrPattern.search(inputStr)
+	subStr = inputStr[subStrMatch.span()[0]+1:subStrMatch.span()[1]-1]
+	# print(subStr)
+	outStr = calculate(subStr)
+	afterHandle = inputStr.replace("("+subStr+")",outStr)
+
+	if "(" not in afterHandle:
+		return afterHandle
+	else:
+		return handlePerentheses(afterHandle)
+
+print("__name__: ", __name__)
+
+if __name__ == "__main__":
+	while 1:
+		print("===================")
+		inputStr = input("Enter: \n")
+
+		inputRegex = re.compile(r'[^\d\+\-\*\/\(\).]')
+		if inputRegex.search(inputStr) or (not inputStr[-1].isdigit() and inputStr[-1] != ")"):
+			print("Input string is abnormal, please check.")
+		else:
+			if "(" in inputStr:
+				strAfterHandle = handlePerentheses(inputStr)
+				outStr = calculate(strAfterHandle)
+			else:
+				outStr = calculate(inputStr)
+			print("Final Ouput: ",outStr)
+
+
+
+
+
+
